46895b8d540d3d18e8ec9fed04b9a2f80da17336
Aoudia Fahim <aoudiafahem1@gmail.com>
2023-12-23 00:23:26 -0500

    Initial commit

---
+++ READ Me !!!!.md
@@ -0,0 +1,50 @@
+
+
+# Projet de Site Web
+
+© 2023 Tous droits réservés à Aoudia Fahem
+
+---
+
+## Informations sur le Projet
+
+Bienvenue dans le projet du site web. Ce site a été créé par Aoudia Fahem.
+
+### Super Admin
+
+Le site comprend un compte Super Admin avec les informations suivantes:
+
+---
+### Informations sur le Super Admin
+
+- **Nom d'utilisateur:** superadmin@admin.ca
+- **Mot de passe:** 12345678
+
+### Activation du Compte
+
+Lors de l'inscription des nouveau utilisateur , le compte doit être activé par le Super Admin avant que les utilisateurs puissent se connecter.
+
+### Compte d'Utilisateur de Test
+
+Un compte utilisateur de test a été créé à des fins de démonstration.
+
+- **Nom d'utilisateur:** aoudiafahem@gmail.com
+- **Mot de passe:** 123456
+
+---
+
+## Base de Données
+
+Des modifications ont été apportées à la base de données pour répondre aux besoins du projet. Veuillez noter que certaines conventions ont été ajustées pour assurer la création d'un site web professionnel.
+
+---
+
+## Remarques Additionnelles
+
+Merci de prendre le temps d'examiner ce projet. Des améliorations ont été apportées à la base de données pour répondre aux exigences d'un site web professionnel, même si cela peut entraîner une perte de points. Votre avis est toujours le bienvenu.
+
+Passez d'agréables vacances!
+
+---
+
+**Aoudia Fahem**
---
+++ register.php
@@ -0,0 +1,86 @@
+<?php
+
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	include 'includes/session.php';
+
+	if(isset($_POST['signup'])){
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$repassword = $_POST['repassword'];
+
+		$_SESSION['firstname'] = $firstname;
+		$_SESSION['lastname'] = $lastname;
+		$_SESSION['email'] = $email;
+
+
+
+		if($password != $repassword){
+			$_SESSION['error'] = 'Passwords did not match';
+			header('location: signup.php');
+		}
+		else{
+			$conn = $pdo->open();
+
+			$stmt = $conn->prepare("SELECT COUNT(*) AS numrows FROM users WHERE email=:email");
+			$stmt->execute(['email'=>$email]);
+			$row = $stmt->fetch();
+			if($row['numrows'] > 0){
+				$_SESSION['error'] = 'Email already taken';
+				header('location: signup.php');
+			}
+			else{
+				$now = date('Y-m-d');
+				$password = password_hash($password, PASSWORD_DEFAULT);
+
+				//generate code
+				$set='123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				$code=substr(str_shuffle($set), 0, 12);
+
+				try{
+					$stmt = $conn->prepare("INSERT INTO users (email, password, firstname, lastname, activate_code, created_on) VALUES (:email, :password, :firstname, :lastname, :code, :now)");
+					$stmt->execute(['email'=>$email, 'password'=>$password, 'firstname'=>$firstname, 'lastname'=>$lastname, 'code'=>$code, 'now'=>$now]);
+					$userid = $conn->lastInsertId();
+
+					$message = "
+						<h2>Thank you for Registering.</h2>
+						<p>Your Account:</p>
+						<p>Email: ".$email."</p>
+						<p>Password: ".$_POST['password']."</p>
+						<p>Please click the link below to activate your account.</p>
+						<a href='http://localhost/ecommerce/activate.php?code=".$code."&user=".$userid."'>Activate Account</a>
+					";
+
+
+
+
+
+				}
+				catch(PDOException $e){
+					$_SESSION['error'] = $e->getMessage();
+					header('location: register.php');
+				}
+
+				$pdo->close();
+
+			}
+
+		}
+
+	}
+	else{
+		$_SESSION['error'] = 'Fill up signup form first';
+		header('location: signup.php');
+	}
+	echo '<div style="background: linear-gradient(to right, #ff7e5f, #feb47b); padding: 20px; border-radius: 10px; text-align: center;">';
+	echo '<p style="font-size: 22px; color: #fff; font-weight: bold;">Merci pour votre inscription!</p>';
+	echo '<p style="font-size: 18px; color: #fff;">Vous devez patienter jusqu\'à ce que l\'administrateur super activé votre compte.</p>';
+	echo '<a href="index.php" style="display: inline-block; background-color: #4CAF50; color: white; padding: 14px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 5px;">';
+	echo '<i class="fa fa-home"></i> Accueil</a>';
+	echo '</div>';
+	
+	
+?>
